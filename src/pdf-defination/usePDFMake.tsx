1820a00bfca46f2ab4575433d63f79416594bd92
Pradeep Raj Thapaliya <pradeep.thapaliya95@gmail.com>
2022-12-26 18:19:00 -0500

    _

--- src/pdf-defination/usePDFMake.tsx
+++ src/pdf-defination/usePDFMake.tsx
@@ -1,5 +1,5 @@
-import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
+import * as pdfFonts from "pdfmake/build/vfs_fonts.js";
+import pdfMake from "pdfmake/build/pdfmake"; 
 import {
   getTextareaInput,
   getTextInput,
